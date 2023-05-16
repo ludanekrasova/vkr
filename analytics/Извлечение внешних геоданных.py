1be47d8a827f5dd7105cb554a082f50194411f5a
ludanekrasova <79700631+ludanekrasova@users.noreply.github.com>
2023-05-17 00:07:45 +0500

    Update Извлечение внешних геоданных.py

--- analytics/Извлечение внешних геоданных.py
+++ analytics/Извлечение внешних геоданных.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[84]:
-
-
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
-
-# In[85]:
-
-
 # Данные по уникальным точкам
 uniq_point = pd.read_csv('data/uniq_point.csv', sep = ',')
-uniq_point.head(3)
-
-
-# In[86]:
-
 
 # Наши точки
 X_zone = uniq_point[['lat','lon']].values
-
-
-# In[87]:
-
 
 # Функция для нахождения блюжайших точек в радиусе
 def r_neighbors(X_train, r=0.01):
@@ -34,10 +18,6 @@ def r_neighbors(X_train, r=0.01):
     neigh.fit(X_train)
     distances, indexes = neigh.radius_neighbors(X_zone, r)
     return np.array([len(x) for x in indexes])
-
-
-# In[88]:
-
 
 # Туалеты
 toilet = pd.read_csv('геоданные/842_norm/842.csv', sep = ';', index_col=0)
@@ -48,10 +28,6 @@ toilet['LAT'] = toilet['LAT'].str.replace("'", "").str.replace(",", ".").astype(
 X_train = toilet[['LAT', 'LON']].values
 uniq_point['toilet'] = r_neighbors(X_train, r=0.01)
 
-
-# In[89]:
-
-
 # Торговля и общепит стационарные
 riteil = pd.read_csv('геоданные/586_norm/586.csv', sep = ';', index_col=0)
 riteil.columns = ['NAME', 'ADDRESS', 'lat', 'lon', 'NAME_OPF', 'LIC_SERNUM', 'LIC_START', 'LIC_STOP','LIC_STATUS']
@@ -59,19 +35,11 @@ riteil.columns = ['NAME', 'ADDRESS', 'lat', 'lon', 'NAME_OPF', 'LIC_SERNUM', 'LI
 X_train = riteil[['lat', 'lon']].values
 uniq_point['riteil'] = r_neighbors(X_train, r=0.01)
 
-
-# In[90]:
-
-
 # Бытовые услуги
 service = pd.read_excel('геоданные/быт_усл-4272-2021-06-09.xlsx')
 # Количество точек бытовых в радиусе 1 км
 X_train = service[['Latitude_WGS84', 'Longitude_WGS84']].values
 uniq_point['service'] = r_neighbors(X_train, r=0.01)
-
-
-# In[91]:
-
 
 # Станции метро
 metro = pd.read_csv('геоданные/624_norm/624.csv', sep = ';', index_col=0)
@@ -79,19 +47,11 @@ metro = pd.read_csv('геоданные/624_norm/624.csv', sep = ';', index_col=
 X_train = metro[['LAT', 'LON']].values
 uniq_point['metro'] = r_neighbors(X_train, r=0.01)
 
-
-# In[92]:
-
-
 # Объекты для аренды
 arenda = pd.read_csv('геоданные/1113_norm/1113.csv', sep = ';', index_col=0)
 # Количество свободных помещений в радиусе 5 км
 X_train = arenda[['LAT', 'LON']].values
 uniq_point['arenda'] = r_neighbors(X_train, r=0.05)
-
-
-# In[93]:
-
 
 # Клубы
 club = pd.read_csv('геоданные/493_norm/493.csv', sep = ';', index_col=0)
@@ -99,19 +59,11 @@ club = pd.read_csv('геоданные/493_norm/493.csv', sep = ';', index_col=0
 X_train = club[['LAT', 'LON']].values
 uniq_point['club'] = r_neighbors(X_train, r=0.02)
 
-
-# In[94]:
-
-
 # Больницы
 med = pd.read_csv('геоданные/502_norm/502.csv', sep = ';', index_col=0)
 # Количество больниц в радиусе 5 км
 X_train = med[['LAT', 'LON']].values
 uniq_point['med'] = r_neighbors(X_train, r=0.05)
-
-
-# In[95]:
-
 
 # Ветбольницы
 vet = pd.read_csv('геоданные/1193_norm/1193.csv', sep = ';', index_col=0)
@@ -121,31 +73,10 @@ vet['LAT'] = vet['LAT'].str.replace("'", "").str.replace(",", ".").astype(float)
 X_train = vet[['LAT', 'LON']].values
 uniq_point['vet'] = r_neighbors(X_train, r=0.05)
 
-
-# In[96]:
-
-
 # Стоматологи
 stomat = pd.read_csv('геоданные/518_norm/518.csv', sep = ';', index_col=0)
 # Количество стоматологий в радиусе 5 км
 X_train = stomat[['LAT', 'LON']].values
 uniq_point['stomat'] = r_neighbors(X_train, r=0.05)
 
-
-# In[97]:
-
-
-uniq_point.head(3)
-
-
-# In[99]:
-
-
 uniq_point[['ap_mac', 'toilet', 'metro', 'arenda', 'riteil', 'service', 'club', 'med', 'vet', 'stomat']].to_csv('data/df_geo.csv', index=False)
-
-
-# In[ ]:
-
-
-
-

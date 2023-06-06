bc7687ea70ec03435358cede4dfe4aa900c4d231
ludanekrasova <79700631+ludanekrasova@users.noreply.github.com>
2023-06-06 18:27:12 +0500

    Add files via upload

--- static/sidebars.js
+++ static/sidebars.js
@@ -10,12 +10,10 @@
 
 // Map your choices to your option value
 var lookup = {
-   '1': ['Супермаркет', 'Магазин у дома', 'Магазин спиртных напитков', 'Кондитерский магазин', 'Магазин здоровой пищи'],
-   '2': ['Парикмахерская', 'Салон красоты', 'Спа салон'],
+   '1': ['Супермаркет', 'Магазин спиртных напитков', 'Кондитерский магазин', 'Магазин здоровой пищи'],
+   '2': ['Парикмахерская', 'Спа салон', 'Ремонт обуви', 'Ремонт ювелирных изделий', 'Фото на документы'],
    '3': ['Ресторан', 'Кафе', 'Бар', 'Ночной клуб'],
-   '4': ['Бытовая химия', 'Магазин косметики', 'Магазин одежды'],
-   '5': ['Клиника', 'Стоматологическая клиника', 'Ветеринарная клиника'],
-   '6': ['Кофе на вынос', 'Ремонт обуви', 'Ремонт ювелирных изделий', 'Автомойка' , 'Фото на документы', 'Газетный киоск', 'Киоск мороженного', 'Химчистка'],
+   '4': ['Клиника', 'Стоматологическая клиника', 'Ветеринарная клиника'],
 };
 
 // When an option is changed, search the above for matching choices
@@ -32,3 +30,39 @@ $('#options').on('change', function() {
       $('#choices').append("<option value='" + lookup[selectValue][i] + "'>" + lookup[selectValue][i] + "</option>");
    }
 });
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
+
+department = getQueryVariable('department');
+
+if (department) {
+
+   var selectValue = department;
+   var selectOption = getQueryVariable('branch').replace("+"," ");
+
+   // Empty the target field
+   $('#choices').empty();
+   
+   // For each chocie in the selected option
+   for (i = 0; i < lookup[selectValue].length; i++) {
+      // Output choice in the target field
+
+      var selected = '';
+      if (selectOption == lookup[selectValue][i]) {
+          selected = ' selected'
+      }
+
+      $('#choices').append("<option value='" + lookup[selectValue][i] + "'"+selected+">" + lookup[selectValue][i] + "</option>");
+   }
+}
+
+
